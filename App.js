2c1e4908da82c9cbd793a34df6400f61f5994c1d
Keelen Fisher <keelencarrera@gmail.com>
2022-11-14 23:04:39 -0600

    demo code added

--- App.js
+++ App.js
@@ -1,14 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Linking, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import * as Contacts from 'expo-contacts';
+import { NativeBaseProvider, Box } from "native-base";
+import { Button } from "native-base";
+
 
 export default function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      const { data } = await Contacts.getContactsAsync({})
+      console.log(data);
+      setContacts(data);
+    }
+    getContacts();
+  }, []);
+
+
+  const call = (contact) => {
+    let phoneNumber = contact.phoneNumbers[0].number
+
+    const link = `tel:${phoneNumber}`;
+    Linking.canOpenURL(link)
+      .then(supported => Linking.openURL(link))
+      .catch(err => console.error(err));
+  }
+
+  const renderItem = ({ item }) => (
+    <Button onPress={() => call(item)} mb="2.5">{item.name}</Button>
+  )
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <SafeAreaView>
+          <View>
+            <Text>Contacts</Text>
+          </View>
+          <View>
+            <FlatList
+              data={contacts}
+              renderItem={renderItem}
+              keyExtractor={item => item.id}
+            />
+          </View>
+          <StatusBar style="auto" />
+        </SafeAreaView>
+      </View>
+    </NativeBaseProvider>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +63,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  header: {
+    marginTop: 100,
+  }
 });
